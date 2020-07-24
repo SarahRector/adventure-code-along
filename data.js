7b98f6234733b628055e44f74771fd40ac5977ab
SarahRector <rector.sarah.r@gmail.com>
2020-07-24 11:27:26 -0700

    added metadata for two quests

--- data.js
+++ data.js
@@ -1,100 +1,76 @@
-const monsters = {
-    id: 'monsters',
-    title: 'A Den of Monsters',
+const creature = {
+    id: 'creature',
+    title: 'The Creature from the Murky Pond',
     map: {
         top: '89%',
         left: '44%'
     },
-    image: 'https://placekitten.com/200/300',
+    image: '../assets/creature-from-the-black-lagoon-zapista-zapista.jpg',
     description: `
-        You enter the quest chamber only to be confronted by a hoard of
-        monsters. And they look hungry. What do you do?
+        Today is the first day of your socially distanced vacation!  You've been waiting for months to spend more time alone, but at a lake! You let yourself into your cabin that you were told was thouroughly cleaned and go ahead and wipe everything down with Clorox wipes again.  You wash a wine glass three times, pour yourself some wine and head out to the deck with the chair that you brought from home.  The sun is just setting, the pond is beautiful. You are just unwinding when you notice something that looks like a giant fin, coming towards you from across the pond.... What do you do?
     `,
     choices: [{
-        id: 'negotiate',
-        description: 'Negotiate with them',
+        id: 'ignore',
+        description: 'Ignore the fin and pretend you dont see it',
         result: `
-            Knowing the monsters are not too bright, you offer to go buy them all
-            turkey dinners from the village pub. They give you 35 gold for meals
-            that will never be delivered. I hope you can live with yourself. 
+            You ignore what you saw, prefering to bury your head in the sand like you father does about the state of our country. The fin creeps closer and a giant floppy monster steps out of the pond to make sure you are registered to vote at your current address.  You are not, so you lose 15 hp but you allow the creature to register you and offer it a glass of wine so they reward you with 20 pieces of gold. 
+        `,
+        hp: -15,
+        gold: 20
+    }, {
+        id: 'poke',
+        description: 'Walk down to the pond and poke it with a stick',
+        result: `
+            You stumble down to the pond, pick up a branch and prod at the fin with a stick. A gilled, slimy creature emerges from the muck, slowly stretching to it's towering height and says, "ow! Really? Poking with a stick? Who raised you anyway?" and swims off. You miss out on making friends with a new species and feel bad so you donate 30 pieces of gold to a charity that you don't take the time to research.
         `,
         hp: 0,
-        gold: 35
+        gold: -30
     }, {
-        id: 'fight',
-        description: 'Fiiiiiggghhhttt!',
+        id: 'invite',
+        description: 'Investigate and invite',
         result: `
-            Brandishing your sword you let out a warrior's cry and charge into the monsters
-            hacking and slashing. Before long you stand panting gazing across the bodies of
-            your vanquished foes. The bad news is you take 30 hp damage. The good news is you
-            find 50 gold.
+            You walk down to the pond and investigate from a safe distance of 6-10 feet. A creature emerges and says "oh hi, I'm Steve". "Hi Steve" you reply. You ask Steve how long he's been quarantiening in the pond and when he replies that it's been three weeks, you offer him a facemask and a glass of wine and the two of you sit on your deck and chat about comic books you've recently read.  You make a friend! Which makes you feel slightly better about things and you gain 25 mental health hp.
         `,
-        hp: -30,
-        gold: 50
-    }, {
-        id: 'run',
-        description: 'Run away like good Sir Robin',
-        result: `
-            As you make a dash for the door a giant spider descends and take a bite of flesh,
-            causing 50 hp damage.
-        `,
-        hp: -50,
+        hp: 25,
         gold: 0
     }]
 };
 
-const dragon = {
-    id: 'dragon',
-    title: 'A Problem Dragon',
+const space = {
+    id: 'space',
+    title: 'Space Jam 5000',
     map: {
         top: '57%',
         left: '67%'
     },
-    image: 'https://placekitten.com/300/300',
-    audio: 'dragon.wav',
-    action: 'dragon-growl.aiff',
+    image: '../assets/space-image.jpg',
     description: `
-        You travel to a nearby village you have heard is being
-        terrorized by a dragon. Sure enough as you rent a room
-        in a local inn, you go outside and see the dragon about
-        to lay seige! What do you do?
+        You have been selected to play on the first intergalactic space roller derby team! You could not be more excited and you spend weeks making sure you pack all of your best space-boutfits along with all of your gear. You meet up with your team and board the space-craft that will take you to your first bout with your new team, the Earth-Rollers. When you land, you notice that the gravity is different and you feel heavier, like youre being pressed into the planets surface. You have 30 minutes with your new team to get ready, what do you do?
     `,
     choices: [{
-        id: 'run',
-        description: 'Get the hell out of the village',
+        id: 'eat',
+        description: 'Sample the local space food',
         result: `
-            You high tail it in the opposite direction. Luckily,
-            in the panic you find a bag on the ground with 15 gold.
-            Unluckily, you trip over a discarded wagon wheel on your
-            way out of town and take 40 hp damage. 
+            You're on a new planet!! There's no way that you're not eating all of the food you can get your hands on. You and your teammate, Sci-Fi Curious, head off to look at several food stalls that are selling things you've never seen before. You settle on a bowl of shiny purple orbs with a shiny gold sauce on them. As you eat them you realize that they're much chewier than you expected and you start to feel light headed. You look down and realize that your feet have swelled to three times their normal size. Your skates don't fit! You traveled all this way and now you can't even play! You lose 10 hp for making your feet bigger and 15 pieces of gold for the space-snack.
         `,
-        hp: -35,
-        gold: 35
+        hp: -10,
+        gold: -15
     }, {
-        id: 'fight',
-        description: 'Fiiiiiggghhhttt!',
+        id: 'wheels',
+        description: 'Change your wheels',
         result: `
-            You attempt to charge towards the dragon, who sees you approach
-            and let's loose a fireball. You wake up the next morning and the
-            village has been completely burned to the ground.
-            Oh, and you take 45 hp damage.
+            This gravity has got you real confused! How are you supposed to skate like this?!?! Did you even bring the right wheels??? You spend 47 minutes changing wheels and testing them, changing your wheels and testing them. All of your work and stress pays off and by the time the first whistle blows you feel like you've really dialed it in. You score 25 points in the first jam and gain 30 hp!
         `,
-        hp: -45,
+        hp: 30,
         gold: 0
     }, {
-        id: 'archer',
-        description: 'Emulate that guy from LOR who shot an arrow',
+        id: 'merch',
+        description: 'You set up the merch table',
         result: `
-            Inspired by the legend of Bard the Bowman, you notice a
-            stunned archer standing nearby and take their bow and quiver,
-            climb to the top of a tall tower and take aim. On the dragon's
-            next pass you steady your aim and let one fly. Amazingly,
-            you strike the dragon in the eye, piercing into the brain and
-            killing the dragon instantly. The villagers declare you their hero
-            and award you 90 gold.
+            Everyone on your team is freaking out, as per usual. Poundstooth is eating tums and deep breathing, Halley Tosis is chatting up the other teams jammers, ManaTease is alternatley stretching and pacing back and forth. You feel like you have your stuff together so you decide to set up the merch table early.  Thanks to your thoughtfulness, your team makes 50 gold pieces befor the bout even starts! You'll all get those matching denim vests after all!
         `,
         hp: 0,
-        gold: 90
+        gold: 50
     }]
 };
 
@@ -137,9 +113,9 @@ const treasure = {
 };
 
 const quests = [
-    monsters, 
+    creature, 
     treasure,
-    dragon
+    space
 ];
 
 export default quests;
